bf4d74e5e22b4160f39ed36a3dbc58b5eeba0c90
alexmeninf <alex.menin11@gmail.com>
2021-08-19 15:39:56 -0300

    Feat: added Grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: [
+          "assets/plugins/jquery/jquery.min.js",
+          "assets/js/util.js",
+          // 'assets/plugins/popper.min.js',
+          // 'assets/plugins/bootstrap/js/bootstrap.min.js',
+          "assets/plugins/jarallax/jarallax.min.js",
+          // 'assets/plugins/jquery-mask/js/jquery.mask.min.js',
+          "assets/plugins/lazyload.min.js",
+          // 'assets/plugins/lightgallery/js/lightgallery.min.js',
+          // 'assets/plugins/owl-carousel/js/owl.carousel.min.js',
+          "assets/plugins/smooth-scroll.js",
+          // 'assets/plugins/sweetalert/sweetalert2.all.min.js',
+          // 'assets/plugins/tilt.js/tilt.jquery.min.js',
+          //"assets/plugins/wow/js/wow.min.js",
+          // 'assets/js/countdown.js',
+          // 'assets/js/swiped-events.min.js',
+          "assets/plugins/scroll-magic/plugins/gsap.min.js",
+          "assets/plugins/scroll-magic/ScrollMagic.min.js",
+          "assets/plugins/scroll-magic/plugins/animation.gsap.min.js",
+          "assets/js/main.js",
+        ],
+        dest: "assets/js/built.min.js",
+      },
+    },
+    watch: {
+      js: {
+        files: ["assets/**/**/*.js"],
+        tasks: ["concat:js"],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.registerTask("default", ["concat", "watch"]);
+};
