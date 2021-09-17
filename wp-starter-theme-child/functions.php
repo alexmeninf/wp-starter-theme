e6a67a02c22df7d8afff52fdcf0d214b2cff7ac7
alexmeninf <alex.menin11@gmail.com>
2021-09-17 17:06:31 -0300

    Fix: improviments

--- wp-starter-theme-child/functions.php
+++ wp-starter-theme-child/functions.php
@@ -8,13 +8,68 @@
  * 
  */
 
+/**
+ * CSS Files
+ */
+new_css('animate-default', 'assets/plugins/wow/css/animate.css');
+new_css('bootstrap-grid', 'assets/plugins/bootstrap/css/bootstrap-grid.css');
+new_css('bootstrap-reboot', 'assets/plugins/bootstrap/css/bootstrap-reboot.css');
+new_css('bootstrap-utilities', 'assets/plugins/bootstrap/css/bootstrap-utilities.css');
+new_css('bootstrap', 'assets/plugins/bootstrap/css/bootstrap.css');
+new_css('fontawesome-default', 'assets/plugins/fontawesome/css/all.min.css');
+new_css('main-default', 'assets/css/main.css');
+new_css('style-default', 'style.css');
 
 /**
- * Chamada do estilo do tema pai
+ * Use CSS Default
+ */
+// use_css('animate-default');
+// use_css('bootstrap-grid');
+// use_css('bootstrap-reboot');
+// use_css('bootstrap-utilities');
+use_css('bootstrap');
+use_css('fontawesome-default');
+use_css('main-default');
+use_css('style-default');
+
+
+/**
+ * Scripts Files 
+ */
+new_js('jquery-default', 'assets/plugins/jquery/jquery.min.js', false);
+new_js('utilities', 'assets/js/util.js', true);
+new_js('bootstrap-default', 'assets/plugins/bootstrap/js/bootstrap.min.js', true);
+new_js('jquery.mask-default', 'assets/plugins/jquery-mask/js/jquery.mask.min.js', true);
+new_js('lazyload-default', 'assets/plugins/lazyload.min.js', true);
+new_js('sweetalert-default', 'assets/plugins/sweetalert/sweetalert2.all.min.js', true);
+new_js('wow-default', 'assets/plugins/wow/js/wow.min.js', true);
+new_js('countdown', 'assets/js/countdown.js', true);
+new_js('swiped-events', 'assets/js/swiped-events.min.js', true);
+new_js('smooth-scroll', 'assets/plugins/smooth-scroll.js', true);
+new_js('main-default', 'assets/js/main.js', true);
+
+/**
+ * Use JS Default
+ */
+use_js('jquery-default');
+// use_js('utilities');
+// use_js('bootstrap-default');
+// use_js('jquery.mask-default');
+// use_js('lazyload-default');
+// use_js('sweetalert-default');
+// use_js('wow-default');
+// use_js('countdown');
+//use_js('swiped-events');
+use_js('smooth-scroll');
+use_js('main-default');
+
+
+/**
+ * Chamada do estilo do tema filho
  */
 add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
 
 function enqueue_parent_styles()
 {
-  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
 }
